73ec571d7fc6f7f9d2fda0e56950da712deca09c
Mr-Arzi <goop.robecarlos@gmail.com>
2025-04-18 23:15:30 -0600

    Update callrestapi.js

--- callrestapi.js
+++ callrestapi.js
@@ -1,5 +1,5 @@
-var url = "http://localhost:8080/api/users";
-//var url = "https://restapi-mrarzi.onrender.com/api/users";
+//var url = "http://localhost:8080/api/users";
+var url = "https://restapi-mrarzi.onrender.com/api/users";
 
 function postUser() {
 	const myId = $('#userId').val();
