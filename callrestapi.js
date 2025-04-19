b0e4739802a3c2e2eb08c6ed05e80ed956669f8c
Mr-Arzi <goop.robecarlos@gmail.com>
2025-04-18 23:08:43 -0600

    update frontend rest api

--- callrestapi.js
+++ callrestapi.js
@@ -1,35 +1,57 @@
-//var url = "http://localhost:3300/api/users";
-var url = "https://restapi-mrarzi.onrender.com/api/users";
+var url = "http://localhost:8080/api/users";
+//var url = "https://restapi-mrarzi.onrender.com/api/users";
 
-function postUser(){
-	console.log(url);
-
-	var myName = $('#name').val();
-	var myEmail = $('#email').val();
-	var myAge = $('#age').val();
-	var myComments = $('#comments').val();
-
-	var myuser = {
-		name: myName,
-		email: myEmail,
-		aage: myAge,
-		comments: myComments
+function postUser() {
+	const myId = $('#userId').val();
+	const myName = $('#name').val();
+	const myEmail = $('#email').val();
+	const myAge = $('#age').val();
+	const myComments = $('#comments').val();
+  
+	const myuser = {
+	  name: myName,
+	  email: myEmail,
+	  age: myAge,
+	  comments: myComments
 	};
-
-	console.log(myuser);
-
-	$.ajax({
-		url: url,
-		type: 'post',
+  
+	if (myId) {
+	  // UPDATE
+	  $.ajax({
+		url: `${url}/${myId}`,
+		type: 'PUT',
 		dataType: 'json',
 		contentType: 'application/json',
-		succes: function (data) {
-			console.log(data);
-			$('#resultado').html(JSON.stringify(data.user));
+		data: JSON.stringify(myuser),
+		success: function (data) {
+		  alert("Usuario actualizado correctamente.");
+		  resetForm();
+		  getUsers();
 		},
-		data: JSON.stringify(myuser)
-	});
-}
+		error: function () {
+		  alert("Error al actualizar.");
+		}
+	  });
+	} else {
+	  // CREATE
+	  $.ajax({
+		url: url,
+		type: 'POST',
+		dataType: 'json',
+		contentType: 'application/json',
+		data: JSON.stringify(myuser),
+		success: function (data) {
+		  alert("Usuario creado correctamente.");
+		  resetForm();
+		  getUsers();
+		},
+		error: function () {
+		  alert("Error al crear.");
+		}
+	  });
+	}
+  }
+  
 
 function getUsers() {
     console.log(url);
@@ -44,21 +66,65 @@ function getUsers() {
 
         arrUsers.forEach(function(item) {
             console.log(item);
-            htmlTableUsers += '<tr>' +
-                                '<td>' + item.id + '</td>' +
-                                '<td>' + item.name + '</td>' +
-                                '<td>' + item.email + '</td>' +
-                                '<td>' + item.age + '</td>' +
-                                '<td>' + item.comments + '</td>' +
-                              '</tr>';
+            htmlTableUsers += `<tr>
+  <td>${item.id}</td>
+  <td>${item.name}</td>
+  <td>${item.email}</td>
+  <td>${item.age}</td>
+  <td>${item.comments}</td>
+  <td>
+    <button onclick="editUser(${item.id})">Editar</button>
+    <button onclick="deleteUser(${item.id})">Eliminar</button>
+  </td>
+</tr>`;
+
         });
 
         htmlTableUsers += '</table>';
 
         $('#resultado').html(htmlTableUsers);
-    });
+    });	
 }
 
+function deleteUser(id) {
+	if (confirm(`¿Estás seguro de eliminar el usuario con ID ${id}?`)) {
+	  $.ajax({
+		url: `${url}/${id}`,
+		type: 'DELETE',
+		success: function (res) {
+		  alert(`Usuario ${id} eliminado correctamente.`);
+		  getUsers(); // refrescar lista
+		},
+		error: function (err) {
+		  alert('Error al eliminar usuario');
+		  console.error(err);
+		}
+	  });
+	}
+  }
 
+  function editUser(id) {
+	// Buscar datos del usuario desde la lista actual
+	$.getJSON(`${url}`, function (res) {
+	  const user = res.users.find(u => u.id === id);
+	  if (user) {
+		$('#name').val(user.name);
+		$('#email').val(user.email);
+		$('#age').val(user.age);
+		$('#comments').val(user.comments);
+		$('#userId').val(user.id); // Campo oculto para saber si es update
+		$('#submitBtn').text('Actualizar');
+	  }
+	});
+  }
 
+  function resetForm() {
+	$('#name').val('');
+	$('#email').val('');
+	$('#age').val('');
+	$('#comments').val('');
+	$('#userId').val('');
+	$('#submitBtn').text('Guardar');
+  }
+  
 
